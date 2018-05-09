9e9ab45711a7642e4592dbfeabd3871d0e6cc7cc
ravi.adhikari <ravi.adhikari@aayulogic.com>
2018-05-09 10:39:58 +0545

    Create Regex Parser for extracting Keyword and Confidence from LDA result

---
+++ chatbot/WordWeightExtractor.py
@@ -0,0 +1,28 @@
+import re
+
+
+def filter_weights(pre_result, confidence):
+    selected_data = list()
+    # Received Data Format
+    # [topic]: topic=> {word:value}: word=>keyword as topics, value=>relevancy of keyword as topic
+    for topic in pre_result:
+        dix = dict()
+        for word, its_value in topic.items():
+            if its_value >= confidence:
+                dix[word] = its_value
+        if dix:
+            selected_data.append(dix)
+    return selected_data
+
+
+def get_weights(stub_data):
+    all_data = list()
+    word_value_finder = re.compile(r'(\d\.\d+)\*\"(\w+)')
+    for row_data in stub_data:
+        dic = dict()
+        values = re.findall(word_value_finder, row_data[1])
+        for value, key in values:
+            dic[key] = float(value)
+        all_data.append(dic)
+    return all_data
+
