98c44fd1ff007851bcb33d9fa62f735c6deb67b7
Ravi Adhikari <ravi.adhikari@aayulogic.com>
2018-05-14 15:37:30 +0545

    Add FAQ Data

--- rule_based/chatbot/faq_data.py
+++ rule_based/chatbot/faq_data.py
@@ -2,54 +2,30 @@ from .utilities import dict_key_tuple
 
 
 user_input = {
-    'hi': ["""Hello to you too. Ask stuff about merojob."""],
-
-    'hello': ["""Hello to you too. Ask stuff about merojob."""],
-
-    'register': ["""Registration is easy. Just sign up for a new account by filling in the form completely with your 
-                 full name, a valid email id, mobile number, preferred job category and password. Click “Create my 
-                 Account” and look for an email from us confirming your registration. Click the link in the email 
-                 and you are now registered with merojob. You can also sign up with Facebook and twitter."""],
-
-    'merojob': ["""merojob is a platform to find jobs of your choice. It is the leading career management site in 
-                Nepal. It has started its venture since November 2009. Even if you are not actively looking for a 
-                job, it can help you manage your career. This jobsite is a total recruitment solution to employers
-                &amp; career management solutions for all jobseekers."""],
-
-    'login': [""" Logging in is easy. Once you sign up, you have to provide your same exact email and password 
-              credentials for you to be able to log in."""],
-
-    'apply': ["""You can apply to any of your relevant job by simply clicking on Apply Now button."""],
-
-    'bank': ["""If you are selected for the applied bank vacancy, Real solution or the concerned bank will inform you 
-            via email and SMS. You'll also get a tracking code is number which is given automatically after the 
-            successful applying procedure. It helps you to track your applications status."""],
-
-    'card': ["""Please open the link that you have been provided in the SMS or Email. Insert your username and password,
-            after the confirmation of attending the exam, please download the admit card."""],
-
-    'location': ["""Of course, you need to provide your preferred job location during the time of registration. """],
-
-    'cost': ["""No, it does not cost anything to register with merojob. It is absolutely free to register and apply. """],
-
-    'selected': ["""If you have been selected for the applied job, the respective organization will inform you via telephone, sms or Email for further procedure. """],
-
-    'multiple': ["""No, you must apply for the jobs according to your background match and your qualification. """],
-
-    're apply': ["""No, you cannot reapply for the vacancy which you have already applied."""],
-
-    'update': ["""Go to Profile Update section. You have to update your profile at least once in six months to add new skills and experiences."""],
-
-    'photo': ["""Here is how can you upload your photograph • Go to your profile and hover over your profile picture • Click on change picture • Select your photograph, reposition it and choose save. """],
-
-    'change': ["""Here is how you can change your password. • Log in to your merojob profile • Hover over your profile picture • Click change password • Confirm you current password, enter your new password twice and click change password. """],
-
-    'forgot': ["""Here is how to recover your password • Click “Forget Password” given in the “Sign up” box. • Enter 
-                your login email id and click submit. You will receive an email with password reset link."""],
-
-    'address': ["""Please refer to the following address for our office location: Ganapati Bhawan, MinBhawan MainRoad, New Baneshwor, Kathmandu, Nepal Email: info@merojob.com Phone No: +977 1 4106701/02/03 """],
-
-    'outsourcing': ["""Outsourcing refers to a process where two organizations may enter into a contractual agreement involving an exchange of services and payments. It is a contract basis job where one company places the person in the other company as their staff. Real Solutions (Sister Concern of merojob) has the provision of outsourcing services. """],
-
-    'kurakani': ["""Job Kurakani section at merojob is basically a resource centre for job seekers as well as recruiting managers. Through Job Kurakani, merojob aims to provide useful career related tips, advice and suggestions to its readers. Job seekers can get useful information on job search guidelines, career development tips, training and education etc. while recruiting managers can get an insight on tips for conducting HR tasks effectively. To learn more about Job Kurakani, click here. https://merojob.com/jobkurakani/ """]
+        'hi': ["""Hello to you too. Ask stuff about merojob."""],
+        'hello': ["""Hello to you too. Ask stuff about merojob."""],
+        'register': ["""Registration is easy. Just sign up for a new account by filling in the form completely with your full name, a valid email id, mobile number, preferred job category and password. Click “Create my Account” and look for an email from us confirming your registration. Click the link in the email and you are now registered with merojob. You can also sign up with Facebook and twitter."""],
+        'merojob': ["""merojob is a platform to find jobs of your choice. It is the leading career management site in Nepal. It has started its venture since November 2009. Even if you are not actively looking for a  job, it can help you manage your career. This jobsite is a total recruitment solution to employers &amp; career management solutions for all jobseekers."""],
+        'password': ["""Here is how to recover your password • Click “Forget Password” given in the “Sign up” box. • Enter your login email id and click submit. You will receive an email with password reset link."""],
+        'login': [""" Logging in is easy. Once you sign up, you have to provide your same exact email and password credentials for you to be able to log in."""],
+        'apply': ["""You can apply to any of your relevant job by simply clicking on Apply Now button."""],
+        'bank': ["""If you are selected for the applied bank vacancy, Real solution or the concerned bank will inform you via email and SMS. You'll also get a tracking code is number which is given automatically after the successful applying procedure. It helps you to track your applications status."""],
+        'card': ["""Please open the link that you have been provided in the SMS or Email. Insert your username and password, after the confirmation of attending the exam, please download the admit card."""],
+        'category': ["""Job category is a section at merojob that classifies the jobs according to various categories. Some of the categories may be accounting, human resource, banking, administration. This section is created to simplify the job search and posting for both the job seekers &amp; the employers. Through job category, candidates can search for jobs according to their field of interest. This section can be seen at the bottom of the featured jobs."""],
+        'post':["""We have different job posting available like Top Jobs, Hot Jobs and Featured Jobs. For Top Jobs, the vacancy can be posted for 3 days, 7 days and 15 days, depending on your need.\nHot Jobs &amp; Featured Jobs, the vacancies can be published for 7 days or 15 days. However, the length of the ad placement can be extended as per your need."""],
+        'payment': ["""You can pay through online system like e-sewa, nPay or deposit in our bank account. For bank account detail please contact your Client Relation Executive."""],
+        'autoresponder' : ["""Auto responder is the customized auto response. Upon setting the auto responder it is automatically sent to applicant who applies for the advertised job vacancy."""],
+        'industry': ["""We cover all kinds of industries. And there are number of companies from various industries registered at meroJob."""],
+        'location': ["""Of course, you need to provide your preferred job location during the time of registration. """],
+        'cost': ["""No, it does not cost anything to register with merojob. It is absolutely free to register and apply. """],
+        'selected': ["""If you have been selected for the applied job, the respective organization will inform you via telephone, sms or Email for further procedure. """],
+        'multiple': ["""No, you must apply for the jobs according to your background match and your qualification. """],
+        're apply': ["""No, you cannot reapply for the vacancy which you have already applied."""],
+        'update': ["""Go to Profile Update section. You have to update your profile at least once in six months to add new skills and experiences."""],
+        'photo': ["""Here is how can you upload your photograph • Go to your profile and hover over your profile picture • Click on change picture • Select your photograph, reposition it and choose save. """],
+        'change': ["""Here is how you can change your password. • Log in to your merojob profile • Hover over your profile picture • Click change password • Confirm you current password, enter your new password twice and click change password. """],
+        'forgot': ["""Here is how to recover your password • Click “Forget Password” given in the “Sign up” box. • Enter your login email id and click submit. You will receive an email with password reset link."""],
+        'address': ["""Please refer to the following address for our office location: Ganapati Bhawan, MinBhawan MainRoad, New Baneshwor, Kathmandu, Nepal Email: info@merojob.com Phone No: +977 1 4106701/02/03 """],
+        'outsourcing': ["""Outsourcing refers to a process where two organizations may enter into a contractual agreement involving an exchange of services and payments. It is a contract basis job where one company places the person in the other company as their staff. Real Solutions (Sister Concern of merojob) has the provision of outsourcing services. """],
+        'kurakani': ["""Job Kurakani section at merojob is basically a resource centre for job seekers as well as recruiting managers. Through Job Kurakani, merojob aims to provide useful career related tips, advice and suggestions to its readers. Job seekers can get useful information on job search guidelines, career development tips, training and education etc. while recruiting managers can get an insight on tips for conducting HR tasks effectively. To learn more about Job Kurakani, click here. https://merojob.com/jobkurakani/ """]
 }
