56ad45124b19a1ad814d9bd98b28911daa3c00a2
Akash Adhikari <akashsky1313@gmail.com>
2018-05-14 15:34:33 +0545

    add user data

--- rule_based/chatbot/faq_data.py
+++ rule_based/chatbot/faq_data.py
@@ -16,9 +16,6 @@ user_input = {
                 job, it can help you manage your career. This jobsite is a total recruitment solution to employers
                 &amp; career management solutions for all jobseekers."""],
 
-    'password': ["""Here is how to recover your password • Click “Forget Password” given in the “Sign up” box. • Enter 
-                your login email id and click submit. You will receive an email with password reset link."""],
-
     'login': [""" Logging in is easy. Once you sign up, you have to provide your same exact email and password 
               credentials for you to be able to log in."""],
 
@@ -29,5 +26,30 @@ user_input = {
             successful applying procedure. It helps you to track your applications status."""],
 
     'card': ["""Please open the link that you have been provided in the SMS or Email. Insert your username and password,
-            after the confirmation of attending the exam, please download the admit card."""]
+            after the confirmation of attending the exam, please download the admit card."""],
+
+    'location': ["""Of course, you need to provide your preferred job location during the time of registration. """],
+
+    'cost': ["""No, it does not cost anything to register with merojob. It is absolutely free to register and apply. """],
+
+    'selected': ["""If you have been selected for the applied job, the respective organization will inform you via telephone, sms or Email for further procedure. """],
+
+    'multiple': ["""No, you must apply for the jobs according to your background match and your qualification. """],
+
+    're apply': ["""No, you cannot reapply for the vacancy which you have already applied."""],
+
+    'update': ["""Go to Profile Update section. You have to update your profile at least once in six months to add new skills and experiences."""],
+
+    'photo': ["""Here is how can you upload your photograph • Go to your profile and hover over your profile picture • Click on change picture • Select your photograph, reposition it and choose save. """],
+
+    'change': ["""Here is how you can change your password. • Log in to your merojob profile • Hover over your profile picture • Click change password • Confirm you current password, enter your new password twice and click change password. """],
+
+    'forgot': ["""Here is how to recover your password • Click “Forget Password” given in the “Sign up” box. • Enter 
+                your login email id and click submit. You will receive an email with password reset link."""],
+
+    'address': ["""Please refer to the following address for our office location: Ganapati Bhawan, MinBhawan MainRoad, New Baneshwor, Kathmandu, Nepal Email: info@merojob.com Phone No: +977 1 4106701/02/03 """],
+
+    'outsourcing': ["""Outsourcing refers to a process where two organizations may enter into a contractual agreement involving an exchange of services and payments. It is a contract basis job where one company places the person in the other company as their staff. Real Solutions (Sister Concern of merojob) has the provision of outsourcing services. """],
+
+    'kurakani': ["""Job Kurakani section at merojob is basically a resource centre for job seekers as well as recruiting managers. Through Job Kurakani, merojob aims to provide useful career related tips, advice and suggestions to its readers. Job seekers can get useful information on job search guidelines, career development tips, training and education etc. while recruiting managers can get an insight on tips for conducting HR tasks effectively. To learn more about Job Kurakani, click here. https://merojob.com/jobkurakani/ """]
 }
