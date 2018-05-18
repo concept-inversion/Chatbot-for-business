e3e7bbddf33065bf22044d01dba7605d451dda5a
Ravi Adhikari <ravi.adhikari@aayulogic.com>
2018-05-18 12:27:48 +0545

    Adds fuzzy matching mechanism

--- rule_based/chatbot/lemmatizer.py
+++ rule_based/chatbot/lemmatizer.py
@@ -1,10 +1,13 @@
 import nltk
 from nltk.stem.wordnet import WordNetLemmatizer
+from fuzzwuzzy import fuzz
 
-def select_noun_verbs_base(sentence):                                                                                                                                                 
+def select_noun_verbs_base(sentence, list_only=False):                                                                                                                                                 
     t_sentence = nltk.word_tokenize(sentence)                                                                            
     tags_words = nltk.pos_tag(t_sentence)
     select_words = [(word,tag) for word,tag in tags_words if tag in ('NN','VB')]
+    if list_only:
+        return None
     return select_words
 
 def select_noun_verbs(sentence):                                                                                                                                                      
@@ -18,3 +21,7 @@ def lemmatize_word(word):
     lemma = WordNetLemmatizer()
     return lemma.lemmatize(word)
 
+def fuzzymatcher(question, query, partial=False):
+    if partial:
+        return fuzz.partial_ratio(question, query)
+    return fuzz.ratio(question, query)
