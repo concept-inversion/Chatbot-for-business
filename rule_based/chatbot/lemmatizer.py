169f18ecdc553705d474608aff7b65e6f43cc7d7
Ravi Adhikari <ravi.adhikari@aayulogic.com>
2018-05-18 12:37:49 +0545

    Adds list_option to POS selectors

--- rule_based/chatbot/lemmatizer.py
+++ rule_based/chatbot/lemmatizer.py
@@ -7,14 +7,16 @@ def select_noun_verbs_base(sentence, list_only=False):
     tags_words = nltk.pos_tag(t_sentence)
     select_words = [(word,tag) for word,tag in tags_words if tag in ('NN','VB')]
     if list_only:
-        return None
+        return [word for word,tag in select_words]
     return select_words
 
-def select_noun_verbs(sentence):                                                                                                                                                      
+def select_noun_verbs(sentence, list_only=False):                                                                                                                                                      
     t_sentence = nltk.word_tokenize(sentence)                                                                            
     tags_words = nltk.pos_tag(t_sentence)
     select_words = [(word,tag) for word,tag in tags_words if tag.startswith('NN') or 
         tag.startswith('VB')]
+    if list_only:
+        return [word for word,tag in select_words]
     return select_words
 
 def lemmatize_word(word):
